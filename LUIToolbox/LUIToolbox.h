35df67089f031cb7a9c2e6cc641de625df3bffdc
Ondrej Rafaj <pro@Super-Dictator.local>
2017-07-05 20:42:59 +0100

    adding basic toolbox

---
+++ LUIToolbox/LUIToolbox.h
@@ -0,0 +1,19 @@
+//
+//  LUIToolbox.h
+//  LUIToolbox
+//
+//  Created by Ondrej Rafaj on 05/07/2017.
+//  Copyright © 2017 manGoweb UK Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LUIToolbox.
+FOUNDATION_EXPORT double LUIToolboxVersionNumber;
+
+//! Project version string for LUIToolbox.
+FOUNDATION_EXPORT const unsigned char LUIToolboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LUIToolbox/PublicHeader.h>
+
+
---
+++ LUIToolbox/String+URL.swift
@@ -0,0 +1,14 @@
+//
+//  String+URL.swift
+//  LUIToolbox
+//
+//  Created by Ondrej Rafaj on 05/07/2017.
+//  Copyright © 2017 manGoweb UK Ltd. All rights reserved.
+//
+
+import Foundation
+
+
+public  extension String {
+    
+}
